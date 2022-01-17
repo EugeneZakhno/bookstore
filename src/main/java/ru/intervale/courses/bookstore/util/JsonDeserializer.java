e27463d79a3ddb2e415eee53538d73826acf2170
Eugene Zakhno <zohan1234567@gmail.com>
2022-01-17 23:27:20 +0300

    COURSES-3: fixed jackson deserializer

--- src/main/java/ru/intervale/courses/bookstore/util/JsonDeserializer.java
+++ src/main/java/ru/intervale/courses/bookstore/util/JsonDeserializer.java
@@ -1,8 +1,12 @@
 package ru.intervale.courses.bookstore.util;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.node.IntNode;
+import ru.intervale.courses.bookstore.entities.Book;
 
 import java.io.IOException;
 
@@ -18,9 +22,21 @@ public class JsonDeserializer  extends StdDeserializer<String> {
         super(vc);
     }
 
+  //  @Override
+  //  public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+  //          throws IOException  {
+  //      return jsonParser.getText();
+  //  }
+
+
+    //https://www.baeldung.com/jackson-custom-serialization
     @Override
-    public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-            throws IOException  {
-        return jsonParser.getText();
+    public String deserialize(JsonParser jp, DeserializationContext ctxt)
+            throws IOException, JsonProcessingException {
+        JsonNode node = jp.getCodec().readTree(jp);
+        int id = (Integer) ((IntNode) node.get("id")).numberValue();
+        String itemName = node.get("itemName").asText();
+        int userId = (Integer) ((IntNode) node.get("createdBy")).numberValue();
+        return new Book(id, itemName, new User(userId, null));
     }
 }
