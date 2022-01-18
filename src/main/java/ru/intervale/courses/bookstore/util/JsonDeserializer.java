a8ace74a072c8f82c5d0c5a18d9e89fbbb6ab861
Eugene Zakhno <zohan1234567@gmail.com>
2022-01-19 00:19:14 +0300

    COURSES-3: fixed JsonDeserializer, change return value

--- src/main/java/ru/intervale/courses/bookstore/util/JsonDeserializer.java
+++ src/main/java/ru/intervale/courses/bookstore/util/JsonDeserializer.java
@@ -5,12 +5,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.node.IntNode;
 import ru.intervale.courses.bookstore.entities.Book;
 
 import java.io.IOException;
-
-
+import java.math.BigDecimal;
 
 public class JsonDeserializer  extends StdDeserializer<String> {
 
@@ -22,21 +20,18 @@ public class JsonDeserializer  extends StdDeserializer<String> {
         super(vc);
     }
 
-  //  @Override
-  //  public String deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
-  //          throws IOException  {
-  //      return jsonParser.getText();
-  //  }
-
-
-    //https://www.baeldung.com/jackson-custom-serialization
+ //   https://www.baeldung.com/jackson-deserialization
     @Override
-    public String deserialize(JsonParser jp, DeserializationContext ctxt)
+    public Book deserialize(JsonParser jp, DeserializationContext ctxt)
             throws IOException, JsonProcessingException {
         JsonNode node = jp.getCodec().readTree(jp);
-        int id = (Integer) ((IntNode) node.get("id")).numberValue();
-        String itemName = node.get("itemName").asText();
-        int userId = (Integer) ((IntNode) node.get("createdBy")).numberValue();
-        return new Book(id, itemName, new User(userId, null));
+        Long id = node.get("id").asLong();
+        String isbn = node.get("isbn").asText();
+        String name = node.get("name").asText();
+        String author = node.get("author").asText();
+        int pageNumber = (Integer) node.get("pageNumber").numberValue();
+        double weight = (Double) node.get("weight").numberValue();
+        BigDecimal price = node.get("price").decimalValue();
+        return new Book(id, isbn, name, author, pageNumber, weight, price);
     }
 }
