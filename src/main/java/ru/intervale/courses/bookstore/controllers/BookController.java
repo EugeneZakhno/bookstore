756bfa5acbeee7ee9405e622c6b7dbd324eaa3c4
Eugene Zakhno <zohan1234567@gmail.com>
2022-01-28 00:14:11 +0300

    COURSES-5: added JdbcTemplate with RowMapper

--- src/main/java/ru/intervale/courses/bookstore/controllers/BookController.java
+++ src/main/java/ru/intervale/courses/bookstore/controllers/BookController.java
@@ -1,13 +1,11 @@
 package ru.intervale.courses.bookstore.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.intervale.courses.bookstore.entities.Book;
 import ru.intervale.courses.bookstore.service.BookService;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
@@ -17,24 +15,21 @@ public class BookController {
     private final BookService bookService;
 
     @PostMapping(value = "/add")
-    public Book addBook(@Valid @RequestBody Book book) {
-    return bookService.addBook(book);
+    public int addBook(@Valid @RequestBody Book book) {
+    return bookService.save(book);
    }
 
     @GetMapping(value = "/get")
-    public List<Book> getAllBook() {
-       return bookService.getAllBook();
-    }
+    public List<Book> getAllBooks() { return bookService.findAll(); }
 
-
-    @PostMapping(value = "/edit")
-    public Book editBook(@Valid @RequestBody Book book) {
-        return bookService.editBook(book);
+    @PostMapping(value = "/edit/{id}")
+    public int editBook(@PathVariable Long id, @Valid @RequestBody Book book) {
+        return bookService.edit(id, book);
     }
 
     @PostMapping(value = "/delete/{id}")
     public void deleteBook(@PathVariable Long id) {
-        bookService.deleteBook(id);
+        bookService.delete(id);
     }
 
 }
--- src/main/java/ru/intervale/courses/bookstore/service/BookService.java
+++ src/main/java/ru/intervale/courses/bookstore/service/BookService.java
@@ -1,141 +1,47 @@
 
 package ru.intervale.courses.bookstore.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import ru.intervale.courses.bookstore.entities.Book;
+import ru.intervale.courses.bookstore.repository.BookRepository;
 
-import javax.sql.DataSource;
-import java.math.BigDecimal;
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class BookService extends JdbcDaoSupport {
+@AllArgsConstructor
+public class BookService implements BookRepository {
 
-    final String url = "jdbc:postgresql://ec2-34-242-89-204.eu-west-1.compute.amazonaws.com:5432/d4fs7m9uvscsn1";
-    final String user = "pwfsxwzkaphztb";
-    final String password = "0411039f232bb00ecaea0259e193981fb4ff8a5e7b1ef21d9dd8183c47cf00a8";
+    private final JdbcTemplate jdbcTemplate;
 
-    public BookService(DataSource dataSource) {
-        this.setDataSource(dataSource);
+
+    @Override
+    public List<Book> findAll() {
+        return jdbcTemplate.query("SELECT * FROM book;", new BookRowMapper());
     }
 
-    List<Book> result = new ArrayList<>();
-
-    String SQL_SELECT = "SELECT * FROM book";
-    String SQL_INSER =  "INSERT INTO book VALUES id = ?, isbn=?, name=?, author=?, page_number=?, weight=?, price=?";
-    String SQL_UPDATE = "UPDATE book SET id = ?, isbn=?, name=?, author=?, page_number=?, weight=?, price=?";
-    String SQL_DELETE = "DELETE FROM book WHERE id = ?";
-
-    public Book addBook(Book book) {
-
-        try (Connection connect = DriverManager.getConnection(url, user, password);
-             PreparedStatement preparedStatement = connect.prepareStatement(SQL_INSER)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                String isbn = resultSet.getString("isbn");
-                String name = resultSet.getString("name");
-                String author = resultSet.getString("author");
-                int pageNumber = resultSet.getInt("page_number");
-                double weight = resultSet.getDouble("weight");
-                BigDecimal price = resultSet.getBigDecimal("price");
-
-                book = Book.builder()
-                        .id(id)
-                        .isbn(isbn)
-                        .name(name)
-                        .author(author)
-                        .pageNumber(pageNumber)
-                        .weight(weight)
-                        .price(price)
-                        .build();
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-        return book;
+    @Override
+    public int save(Book book) {
+        return jdbcTemplate.update("INSERT INTO book(id, isbn,name,author,page_number, weight, price)VALUES (?, ?);",
+                book.getId(), book.getIsbn(), book.getName(), book.getAuthor(), book.getPageNumber(), book.getWeight(),
+                book.getPrice());
     }
 
 
-    public List<Book> getAllBook() {
-        try (Connection connect = DriverManager.getConnection(url, user, password);
-             PreparedStatement preparedStatement = connect.prepareStatement(SQL_SELECT)) {
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                long id = resultSet.getLong("id");
-                String isbn = resultSet.getString("isbn");
-                String name = resultSet.getString("name");
-                String author = resultSet.getString("author");
-                int pageNumber = resultSet.getInt("page_number");
-                double weight = resultSet.getDouble("weight");
-                BigDecimal price = resultSet.getBigDecimal("price");
-
-                Book book = Book.builder()
-                        .id(id)
-                        .isbn(isbn)
-                        .name(name)
-                        .author(author)
-                        .pageNumber(pageNumber)
-                        .weight(weight)
-                        .price(price)
-                        .build();
-                result.add(book);
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-        return result;
+    @Override
+    public void delete(Long id) {
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?;", id);
     }
 
-    public Book editBook(Book book) {
+    @Override
+    public int edit(Long id, Book book) {
+        return jdbcTemplate.update("UPDATE Book SET id=?, isbn=?, name=?, author=?, pageNumber=?, weight=?, price=?"
+                        + "WHERE id=?",
+                book.getId(), book.getIsbn(), book.getName(), book.getAuthor(), book.getPageNumber(), book.getWeight(),
+                book.getPrice());
 
-        try (Connection connect = DriverManager.getConnection(url, user, password);
-             PreparedStatement preparedStatement = connect.prepareStatement(SQL_UPDATE)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                String isbn = resultSet.getString("isbn");
-                String name = resultSet.getString("name");
-                String author = resultSet.getString("author");
-                int pageNumber = resultSet.getInt("page_number");
-                double weight = resultSet.getDouble("weight");
-                BigDecimal price = resultSet.getBigDecimal("price");
-
-                book = Book.builder()
-                        .id(id)
-                        .isbn(isbn)
-                        .name(name)
-                        .author(author)
-                        .pageNumber(pageNumber)
-                        .weight(weight)
-                        .price(price)
-                        .build();
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-        return book;
     }
 
-    public void deleteBook(Long id) {
-        try (Connection connect = DriverManager.getConnection(url, user, password);
-             PreparedStatement preparedStatement = connect.prepareStatement(SQL_DELETE)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                id = resultSet.getLong("id");
-            }
-        } catch (SQLException e) {
-            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
-        }
-    }
 
 }
