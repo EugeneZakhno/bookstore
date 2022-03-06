b23a64bdf56e76e6add090e3e578856cd5ebc5b1
Eugene Zakhno <zohan1234567@gmail.com>
2022-03-07 00:36:29 +0300

    COURSES-5: Mockito written

--- src/test/java/ru/intervale/courses/bookstore/controllers/BookControllerTest.java
+++ src/test/java/ru/intervale/courses/bookstore/controllers/BookControllerTest.java
@@ -1,28 +1,83 @@
 package ru.intervale.courses.bookstore.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.intervale.courses.bookstore.entities.Book;
+import ru.intervale.courses.bookstore.repository.BookRepository;
+import ru.intervale.courses.bookstore.service.BookService;
 
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class BookControllerTest {
+
+    @Mock
+    private BookRepository bookRepository;
+    private BookService bookService;
+
+    @BeforeEach
+    void setUp() {
+        bookService = new BookService(bookRepository);
+    }
 
     @Test
     void getAllBookRecords() {
+        bookService.findAllBooks();
+        verify(bookRepository).findAll();
     }
 
     @Test
     void getBookById() {
+        // given
+        Book book = new Book(10L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+                BigDecimal.valueOf(474.25));
+        // when
+        bookService.findById(book.getId());
+        // Then
+        verify(bookRepository).findById(book.getId());
     }
 
     @Test
     void createBookRecord() {
+        // given
+        Book book = new Book(9L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+                BigDecimal.valueOf(474.25));
+        // when
+        bookService.save(book);
+        // then
+        ArgumentCaptor<Book> bookArgumentCaptor = ArgumentCaptor.forClass(Book.class);
+        verify(bookRepository).save(bookArgumentCaptor.capture());
+        Book capturedBook = bookArgumentCaptor.getValue();
+        assertThat(capturedBook).isEqualTo(book);
     }
 
     @Test
     void editBook() {
+        // given
+        Book book = new Book(15L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+                BigDecimal.valueOf(474.25));
+        // when
+        bookService.save(book);
+        // Then
+        verify(bookRepository).save(book);
     }
 
     @Test
     void deleteBook() {
+        // given
+        Book book = new Book(11L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+                BigDecimal.valueOf(474.25));
+        // when
+        bookService.delete(book.getId());
+        // Then
+        verify(bookRepository).deleteBookById(book.getId());
     }
 }
--- src/test/java/ru/intervale/courses/bookstore/service/BookServiceTest.java
+++ src/test/java/ru/intervale/courses/bookstore/service/BookServiceTest.java
@@ -1,18 +1,17 @@
 package ru.intervale.courses.bookstore.service;
 
-import lombok.AllArgsConstructor;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.intervale.courses.bookstore.entities.Book;
 import ru.intervale.courses.bookstore.repository.BookRepository;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 
@@ -35,28 +34,50 @@ class BookServiceTest {
   }
 
   @Test
-  void canAddStudent() {
+  void canAddBook() {
     // given
     Book book = new Book(9L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
                                      BigDecimal.valueOf(474.25));
     // when
     bookService.save(book);
     // then
-
+      ArgumentCaptor<Book> bookArgumentCaptor = ArgumentCaptor.forClass(Book.class);
+      verify(bookRepository).save(bookArgumentCaptor.capture());
+      Book capturedBook = bookArgumentCaptor.getValue();
+      assertThat(capturedBook).isEqualTo(book);
   }
 
-//
- // @Test
- // public void saveMock(){
-//
- //   Book book1 = new Book(9L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
- //                           BigDecimal.valueOf(474.25));
-//
- //   Book book2 = new Book(9L,"dfdhgb-ery", "JUnit 5 In Action","Moris", 520, 421.02,
- //           BigDecimal.valueOf(874.25));
-//
-//
- // }
-//
+  @Test
+  void canEditBook() {
+    // given
+    Book book = new Book(15L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+            BigDecimal.valueOf(474.25));
+    // when
+    bookService.save(book);
+    // Then
+    verify(bookRepository).save(book);
+  }
+
+  @Test
+  void canFindBookById() {
+    // given
+    Book book = new Book(10L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+            BigDecimal.valueOf(474.25));
+    // when
+    bookService.findById(book.getId());
+    // Then
+    verify(bookRepository).findById(book.getId());
+  }
+
+  @Test
+  void canDeleteBook() {
+    // given
+    Book book = new Book(11L,"dfdhgb-ery", "Mockito In Action","ALAN", 500, 321.02,
+            BigDecimal.valueOf(474.25));
+    // when
+    bookService.delete(book.getId());
+    // Then
+    verify(bookRepository).deleteBookById(book.getId());
+  }
 
 }
