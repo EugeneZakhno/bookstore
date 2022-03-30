3517f80df35fb9dfd84242faceb77b8d466148eb
Eugene Zakhno <zohan1234567@gmail.com>
2022-03-30 23:26:45 +0300

    COURSES-6: extract from openLibrary and from database with WebClient

--- src/main/java/ru/intervale/courses/bookstore/service/BookService.java
+++ src/main/java/ru/intervale/courses/bookstore/service/BookService.java
@@ -4,11 +4,9 @@ package ru.intervale.courses.bookstore.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.intervale.courses.bookstore.entities.Book;
 import ru.intervale.courses.bookstore.repository.BookRepository;
-
-import org.springframework.http.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,7 +14,7 @@ import java.util.Optional;
 @Service
 @Transactional
 @AllArgsConstructor
-public class BookService  {
+public class BookService {
 
     BookRepository bookRepository;
 
@@ -24,16 +22,23 @@ public class BookService  {
         return bookRepository.findAll();
     }
 
-    public Book getBook(Long id ) {
-        Optional<Book> bookResponse =  bookRepository.findById(id);
+    public Book getBook(Long id) {
+        Optional<Book> bookResponse = bookRepository.findById(id);
         Book book = bookResponse.get();
         return book;
     }
 
     public String findByAuthorOpenLibrary(String author) {
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("https://openlibrary.org/authors/"+author+".json", String.class);
-        return responseEntity.getBody() + "\n" + findByAuthor(author);
+        WebClient webClient = WebClient.create("https://openlibrary.org");
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/authors/" + author + ".json")
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block()
+                + "\n" + findByAuthor(author);
     }
 
     public List<Book> findByAuthor(String author) {
@@ -41,14 +46,14 @@ public class BookService  {
     }
 
     public Book edit(Book book) {
-       return bookRepository.save(book);
-   }
+        return bookRepository.save(book);
+    }
 
     public Book save(Book book) {
         return bookRepository.save(book);
     }
 
     public void delete(Long id) {
-      bookRepository.deleteBookById(id);
+        bookRepository.deleteBookById(id);
     }
 }
